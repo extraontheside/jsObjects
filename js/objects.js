1a3d0bcd45b572629edebfc0c6d1039732f643a4
Zachary Barnett <extraontheside@hotmail.com>
2016-09-19 11:44:55 -0600

    Add prompts and alerts

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,24 @@
 //Objects lesson.
+var Robot = {
+    fuel: 100,
+    Weapon: "laser",
+    Speed: 10,
+    Strength: 5,
+    Armor: "light",
+
+    AddFuel: function (tank){
+        fuel += tank;
+    },
+
+    UpgradeArmor: function (change) {
+        armor = change;
+    },
+
+    ChangeWeapon: function (change) {
+        weapon = change;
+    },
+}
+
+Robot.speed += parseInt(prompt("Increase Robot speed."));
+alert("Robot now has a speed of " + Robot.speed);
+Robot.UpgradeArmor(prompt("What should we upgrade the armor to?"));
