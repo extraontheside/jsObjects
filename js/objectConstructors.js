4c625b104d3a3484db7f66b83c9ad7961cca2ae2
Zachary Barnett <extraontheside@hotmail.com>
2016-09-21 11:56:38 -0600

    player position

--- js/objectConstructors.js
+++ js/objectConstructors.js
@@ -13,17 +13,17 @@ function player () {
 var player1 = new player();
 var player2 = new player();
 
-function UpdatePlayer (currentPlayer, healthUpdate, ammo) {
+function UpdatePlayer (currentPlayer, healthUpdate, ammo, playerPosition) {
     currentPlayer.name = prompt("Enter name for current player.");
-    alert("Player 1 name is " + currentPlayer.name);
+    alert(playerPosition + "'s name is " + currentPlayer.name);
     currentPlayer.health = healthUpdate;
     currentPlayer.healing = "Weak";
     currentPlayer.ReArm(ammo);
     alert(currentPlayer.name + " has " + currentPlayer.ammoCount + " in their arsonal.");
 }
 
-UpdatePlayer(player1, 20, 1)
-UpdatePlayer(player2, 60, 1000)
+UpdatePlayer(player1, 20, 1, "Player 1")
+UpdatePlayer(player2, 60, 1000, "Player 2")
 
 //player2.name = prompt("Enter name for player2.");
 //alert("player2 name is " + player2.name);
