e1e3de24388143685654992641124362501ac1ef
Zachary Barnett <extraontheside@hotmail.com>
2016-10-14 11:45:21 -0600

    start battle stuff

--- js/mid_term/rpgMidTerm.js
+++ js/mid_term/rpgMidTerm.js
@@ -1,8 +1,8 @@
 function genericRPG () {
 
-alert("Brave villager, you have been choosen to help your village from creatures plundering precious food and gold.") 
+alert("Brave villager, you have been choosen to help your village from creatures plundering precious food and gold.");
 
-//var weapons = ["sword", "staff", "bow"]
+//var weapons = ["sword", "staff", "bow"];
 
 var sword = {
     physicalDamage: 1,
@@ -51,14 +51,14 @@ bestowWeapons(myCharacter);
 //have class after name
 function setName(){
   var name = window.prompt("What is your name?")
-if (!name) {
+    if (!name) {
     name = window.prompt("I don't know what to call you if you don't enter a name. If you don't answer, I will be forced to name you myself.");
 
     if (!name) {
         window.alert("Your name will be Puddles.");
         name = "Puddles";
     }
-}
+    }
   return name;
 }
 
@@ -67,7 +67,16 @@ setStatsBasedOnClass(character);
 function setClass() {
     var cClass = window.prompt("What class would you like to be? (Choose from warrior, spellcaster, or ranger)").toLowerCase();
     myCharacter.characterClass = cClass;
+        if (!cClass) {
+            cClass = window.prompt("What class are you?");
+
+        if (!cClass) {
+            window.alert("You will be a Jester");
+        }
+        }
+        return cClass;
 };
+
 
 function setStatsBasedOnClass(character){
     switch (character.characterClass) {
@@ -84,11 +93,11 @@ function setStatsBasedOnClass(character){
         break;
 
     default:
-    character.characterClass = "Jester"
-    character.strenght = 0
-      character.dexterity = charcter.dexterity + 20
+    character.characterClass = "Jester";
+    character.strenght = 0;
+    //character.dexterity = charcter.dexterity + 20;
         break;
-}
+    }
 }
 
 //document.getElementById("name").innerHTML = character.name;
@@ -101,49 +110,67 @@ alert("Brave " + myCharacter.name + ", you have choosen " + myCharacter.characte
 function bestowWeapons(character) {
     switch (character.characterClass) {
         case "warrior": 
-            character.weapons = sword
+            character.weapons = sword;
             break;
         case "spellcaster":
-            character.weapons = staff
+            character.weapons = staff;
             break;
         case "ranger":
-            character.weapons = bow
+            character.weapons = bow;
         default: 
         character.weapons = rubberChicken
             break;
     }
 }
 
-var enemy1 = {name: "Bat", type: "flying", eHealth: 20, eAttack: 5, eDefense: 5, eMagicDefense: 0}
-var enemy2 = {name: "skeleton", type: "magic", eHealth: 50, eAttack: 10, eDefense: 5, eMagicDefense: 10}
-var enemy3 = {name: "goblin", type: "normal", eHealth: 50, eAttack: 20, eDefense: 10, eMagicDefense: 5}
+var enemy = [bat, skeleton, goblin];
 
-var choice1 = prompt("Brave " + myCharacter.name + ", there are enemies in your village. Where do you want to go? (Choose Farm, Town Hall, or Market)");
+var bat = {name: "Bat", type: "flying", eHealth: 20, eAttack: 5, eDefense: 5, eMagicDefense: 0};
+var skeleton = {name: "skeleton", type: "magic", eHealth: 50, eAttack: 10, eDefense: 5, eMagicDefense: 10};
+var goblin = {name: "goblin", type: "normal", eHealth: 50, eAttack: 20, eDefense: 10, eMagicDefense: 5};
+
+var choice1 = prompt("Brave " + myCharacter.name + ", there are enemies in your village. Where do you want to go? (Choose Farm, Town Hall, or Market)").toLocaleLowerCase();
 
 places(choice1);
 
 function places(area) {
     switch (area) {
         case "farm":
-            alert("You encountered a Skeleton!")
+            alert("You encountered a Skeleton!");
+            prompt
             break;
-        case "Town Hall":
-           alert("You encountered a Goblin!")
+        case "town hall":
+           alert("You encountered a Goblin!");
+           prompt
             break;
         case "market":
-           alert("You encountered a Bat!")
+           alert("You encountered a Bat!");
+           prompt
             break;
     }
     
 }
 
-var health = document.getElementById("health")
-health.value = health.value (add, subtract health, whatever.)
 
-function enemyAttack(damage) {
-    damage = enemy.eAttack - characte.strength
-    if (damage >= 0)
+//var health = document.getElementById("health");
+//health.value = health.value ();
+
+function enemyAttack(defend) {
+    var pDamage = (enemy.eAttack * 5 - characte.strength);
+    if (damage <= 0);
     alert("The " + enemy + " didn't do any damage.")
 }
+
+function playerAttack(attack) {
+    var eDamage = (character.strenght - enemy.eDefense - enemy.eMagicDefense);
+}
+
+function battle(places) { 
+    window.prompt("Brave " + character.name + ", what action will you choose? Attack, magic, or run?").toLowerCase();
+    if (choice === attack) {
+        alert("You attack the " + enemy + " for " + playerAttack + ".")
+    }
+}
+
 
 }
